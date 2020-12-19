dbb00b0dc2c1f0b0d266e2ae1c3e35cb8cb61624
cristian navarro leon <cristiannl.0117@gmail.com>
2020-12-19 05:52:45 -0500

    Landing page

---
+++ src/components/TheCarousel.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner h-100 mt-1">
+            <div class="carousel-item active mt-10">
+                <img class="mx-auto d-block w-75" src="../assets/Clientes1.jpg" alt="First slide" />
+                <div class="carousel-caption d-none d-sm-block">
+                    <p>Actualmente, los clientes no buscan apenas un producto, buscan una experiencia completa, ayuda instantánea en el momento exacto y
+                         sobre todo, un socio de negocios confiable.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="mx-auto d-block w-75" src="../assets/Clientes2.jpg" alt="Second slide" />
+                <div class="carousel-caption d-none d-sm-block">
+                    <p>El servicio de atención al cliente es todo el soporte ofrecido a tus clientes, sea antes, durante o después de la compra, y que los ayuda a tener una excelente experiencia con tu empresa.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="mx-auto d-block w-75" src="../assets/Clientes3.jpg" alt="Third slide" />
+                <div class="carousel-caption d-none d-sm-block">
+                    <p>Para obtener éxito necesitas entender el concepto de atención al cliente y cómo está cambiando, para que seas capaz de desarrollar tu propia manera de relacionarte con el público.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="mx-auto d-block w-75" src="../assets/Clientes4.jpg" alt="Fourth slide" />
+                <div class="carousel-caption d-none d-sm-block">
+                    <p>Las experiencias digitales y multicanales se consolidan como el medio de satisfacción para los consumidores cada vez más conectados, informados y empoderados.</p>
+                </div>
+            </div>
+        </div>
+        <a
+        class="carousel-control-prev"
+        href="#carouselExampleControls"
+        role="button"
+        data-slide="prev"
+        >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+        </a>
+        <a
+        class="carousel-control-next"
+        href="#carouselExampleControls"
+        role="button"
+        data-slide="next"
+        >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+.carousel-caption {
+    color: black;
+    position: relative;
+    width: 70%;
+    top: 0px;
+    left: 0px;
+    margin: 0 auto;
+    padding: 1em;
+}
+
+.carousel-control-next,
+.carousel-control-prev {
+    filter: invert(100%);
+}
+
+.carousel-inner {
+    height: 30vw;
+    border-top: white;
+    margin-top: 2em;
+}
+
+img {
+    height: 33vw;
+}
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="landing">
+    <the-menu></the-menu>
+    <h2 id="us" class="my-3">Nosotros</h2>
+    <the-carousel></the-carousel>
+
+    <h2 id="services">Servicios</h2>
+    <div class="d-flex flex-row justify-content-around flex-wrap mx-3 mt-3 mb-0">
+      <the-article
+      name="Service Management" 
+      description="El sistema Service Management es un robusto software que incorpora las funcionalidades de un ITSM y surge como el siguiente paso evolutivo de un Service Desk."  
+      image="Articulo1.jpg">
+      </the-article>
+
+      <the-article 
+      name="Aplicativo PQRS" 
+      description="Aplicativo para la gestión inteligente de Peticiones, Quejas, Reclamos y Solicitudes PQRS; diseñado para lograr atender con oportunidad y calidad las solicitudes ingresadas por diferentes canales, mantener trazabilidad e identificar puntos débiles para una toma de decisiones asertiva."  
+      image="Articulo2.jpg">
+      </the-article> 
+
+      <the-article
+      name="ChatBot" 
+      description="Robots desarrollados dentro de una aplicación de mensajes con el principio de automatizar la comunicación."  
+      image="Articulo3.jpg">
+      </the-article>
+    </div>
+
+    <h2 id="stories">Casos de éxito !</h2>
+    
+    <div>
+      <the-story
+      name="Julio Nel"
+      imagen="Articulo3.jpg"
+      date="Marzo 2020"
+      story="Proin aliquam turpis nec mi ultricies, eget gravida diam elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur bibendum lectus sit amet ornare aliquam. Donec pharetra augue tellus, eu varius mi tincidunt quis. In hac habitasse platea dictumst. Nullam eget ipsum ligula. Nunc at gravida mi. Praesent maximus ultricies lacus vitae tristique.">
+      </the-story>
+    </div>
+    <div>
+      <the-story
+      name="Oscar"
+      imagen="Articulo3.jpg"
+      date="Marzo 2020"
+      story="Proin aliquam turpis nec mi ultricies, eget gravida diam elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur bibendum lectus sit amet ornare aliquam. Donec pharetra augue tellus, eu varius mi tincidunt quis. In hac habitasse platea dictumst. Nullam eget ipsum ligula. Nunc at gravida mi. Praesent maximus ultricies lacus vitae tristique.">
+      </the-story>
+    </div>
+    <div>
+      <the-story
+      name="William"
+      imagen="Articulo3.jpg"
+      date="Marzo 2020"
+      story="Proin aliquam turpis nec mi ultricies, eget gravida diam elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur bibendum lectus sit amet ornare aliquam. Donec pharetra augue tellus, eu varius mi tincidunt quis. In hac habitasse platea dictumst. Nullam eget ipsum ligula. Nunc at gravida mi. Praesent maximus ultricies lacus vitae tristique.">
+      </the-story>
+    </div>
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+
+import TheCarousel from '../components/TheCarousel.vue'
+import TheMenu from '../components/TheMenu.vue'
+import TheArticle from '../components/TheArticle.vue'
+import TheStory from '../components/TheStory.vue'
+import TheFooter from '../components/TheFooter.vue'
+
+// @ is an alias to /src
+
+export default {
+  name: 'Landing',
+  components: {
+    TheMenu,
+    TheCarousel,
+    TheArticle,
+    TheStory,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+
+body {
+  --width: 90%;
+  --bg-color: #212529;
+  background: lightgray;
+  border-right: solid var(--bg-color) 2px;
+  border-left: solid var(--bg-color) 2px;
+  margin: 0 auto;
+  width: var(--width);
+  padding-top: 70px;
+}
+p {
+  margin: 0px;
+}
+h2{
+  text-align: center;
+}
+
+</style>
